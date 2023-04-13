ecacc8fc559a24cd6672a315741e490770142f7e
anntseng <irenetseng1205200@gmail.com>
2023-04-13 15:26:29 -0700

    Common security issues

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 
 //把route另開檔案存放
@@ -22,9 +27,10 @@ const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
 
 
-//??
+// //??
 const { Console } = require('console');
 const { runInNewContext } = require('vm');
+const ExpressMongoSanitize = require('express-mongo-sanitize');
 
 
 
@@ -55,20 +61,71 @@ app.use(express.urlencoded({ extended: true }))
 //讓express可以用app.put
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-
+//防Mongo Injection
+app.use(mongoSanitize())
 //session for flashing msg
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
         maxAge: 1000 * 60 * 60 * 24 * 3
     }
 }
 app.use(session(sessionConfig))
 app.use(flash());
+// app.use(helmet());
+
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://console.cloudinary.com/console/c-baf5653f5906f2a987b3d15a6a5568/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+
+
 
 
 //passport - authentication tool
@@ -80,7 +137,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session)
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals, error = req.flash('error');
@@ -100,29 +157,10 @@ app.get('/', (req, res) => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 //沒找到URL的頁面都會跳到這
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 });
-
-
-
-
-
-
 
 
 //Error Handler Middleware
